66e0c94c5cab6fccd271a1314cbc4fe0ebfcbdae
Marco-Pellegrino <53620289+Marco-Pellegrino@users.noreply.github.com>
2021-12-31 15:59:50 +0100

    update

---
+++ app.js
@@ -0,0 +1,23 @@
+const image_div = document.createElement("img")
+
+
+    fetch('assets/image_list.json')
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error("HTTP error, status = " + response.status);
+      }
+      return response.json();
+    })
+    .then(function(json) {
+      for(var i = 0; i < json.data.length; i++) {
+        const path = json.data[i].img;
+        console.log(path)
+        image_div.setAttribute("src", path)
+
+        document.body.appendChild(image_div);
+      }
+    })
+
+
+
+
